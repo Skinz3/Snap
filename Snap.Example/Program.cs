9a77e87026bbf836cb30441b580726315b0453e4
Skinz3 <oliviamarius35@gmail.com>
2021-08-15 15:58:49 +0200

    Initial commit. Create VS solution

---
+++ Snap.Example/Program.cs
@@ -0,0 +1,41 @@
+﻿using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+using System;
+
+namespace Snap.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VideoMode mode = new VideoMode(1920, 1080);
+            RenderWindow window = new RenderWindow(mode, "SFML.NET");
+
+            window.Closed += (obj, e) => { window.Close(); };
+            window.KeyPressed +=
+                (sender, e) =>
+                {
+                    Window window = (Window)sender;
+                    if (e.Code == Keyboard.Key.Escape)
+                    {
+                        window.Close();
+                    }
+                };
+
+            Clock clock = new Clock();
+            float delta = 0f;
+            float angle = 0f;
+            float angleSpeed = 90f;
+
+            while (window.IsOpen)
+            {
+                delta = clock.Restart().AsSeconds();
+                angle += angleSpeed * delta;
+                window.DispatchEvents();
+                window.Clear(Color.White);
+                window.Display();
+            }
+        }
+    }
+}
---
+++ Snap/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Snap
+{
+    public class Class1
+    {
+    }
+}
