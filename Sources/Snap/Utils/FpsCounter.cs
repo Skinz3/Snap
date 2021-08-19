0326fc5690798305b79cb863a97fd64a31212413
Skinz3 <oliviamarius35@gmail.com>
2021-08-19 16:16:03 +0200

    Add FPS Counter.

---
+++ Sources/Snap/Utils/FpsCounter.cs
@@ -0,0 +1,38 @@
+﻿using SFML.System;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snap.Utils
+{
+    public class FpsCounter
+    {
+        public float FPS
+        {
+            get;
+            private set;
+        }
+        private float LastTime
+        {
+            get;
+            set;
+        }
+        private Clock Clock
+        {
+            get;
+            set;
+        }
+        public FpsCounter()
+        {
+            this.Clock = new Clock();
+        }
+
+        public void Update()
+        {
+            float currentTime = Clock.Restart().AsSeconds();
+            this.FPS = 1.0f / (currentTime - LastTime);
+            LastTime = currentTime;
+            this.Clock.Restart();
+        }
+    }
+}
